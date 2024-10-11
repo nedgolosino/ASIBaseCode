c1b9876eebb219453acf38823ebfd250e33765cd
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2024-10-11 21:52:21 +0800

    Product CRUD

---
+++ ASI.Basecode.Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using ASI.Basecode.Data.Models;
+
+namespace ASI.Basecode.Services.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int productId);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productId);
+
+    }
+}
